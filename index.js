b5a268aadc018bd58318141a02af1111291ec58d
ATOROYO <davidatoroyosika@gmail.com>
2024-09-04 18:01:51 +0300

    Coded Asynchronously

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-// Synchronous way of reading
+// Synchronous way of reading and writng
 const input = fs.readFileSync("txt/input.txt", "utf-8");
 console.log(input);
 
@@ -14,3 +14,14 @@ fs.writeFileSync(
   "txt/sharon.txt",
   "My girlfriend is called Sharon and I really love her so much and she finally told her grany about me"
 );
+
+// Asynchronous Way of reading and writng files
+fs.readFile(`./txt/start.txt`, "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      fs.writeFile(`./txt/brendy.txt`, ` ${data3}`, (err) => {
+        console.log("I logged out Data 3");
+      });
+    });
+  });
+});
