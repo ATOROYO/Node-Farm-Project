250f1961a1acd793d92a28d2435627e9f2510ba4
ATOROYO <davidatoroyosika@gmail.com>
2024-09-04 17:43:30 +0300

    Coded the readFileSync

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const fs = require("fs");
 
-const input = fs.readFileSync("/txt/input.txt", "utf-8");
+// Synchronous way of reading
+const input = fs.readFileSync("txt/input.txt", "utf-8");
 console.log(input);
+
+const finalText = fs.readFileSync("txt/final.txt", "utf-8");
+console.log(finalText);
+
+const output = `This is what I know about Avocado:${input}\n${Date.now()}`;
+fs.writeFileSync("txt/output.txt", output);
+
+fs.writeFileSync(
+  "txt/sharon.txt",
+  "My girlfriend is called Sharon and I really love her so much and she finally told her grany about me"
+);
